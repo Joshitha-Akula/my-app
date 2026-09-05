329d7ef798f5849e0086bb878b9daef0dd9ba7c3
Joshitha Akula <akulajoshitha32@gmail.com>
2026-09-05 21:18:42 +0530

    update app message

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello CI/CD Pipeline");
+app.get("/", (req, res) => {
+    res.send("Hello CI/CD pipeline");
+});
 });
 
 app.listen(PORT, () => {
